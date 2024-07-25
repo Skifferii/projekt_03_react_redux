fe0967dc805d913bda6a4acad6392ce0fce09904
Skiferii <dolzhenkoandriy@gmail.com>
2024-07-25 19:22:54 +0200

    allDelete

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
-import React from "react"
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
 import App from "./App"
-import { store } from "./app/store"
-import "./index.css"
+import { store } from "./store/store"
 
 const container = document.getElementById("root")
 
@@ -11,11 +9,9 @@ if (container) {
   const root = createRoot(container)
 
   root.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>,
+    <Provider store={store}>
+      <App />
+    </Provider>,
   )
 } else {
   throw new Error(
